3ad69078d24bcf52a0f54308a1c4b9c637b3c19c
jokrokengen <jokrokengen@gmail.com>
2017-11-04 00:08:59 +0100

    react-list

--- wwwroot/react.jsx
+++ wwwroot/react.jsx
@@ -1,35 +1,67 @@
-var Players = React.createClass({
-    getInitialState() {
-       dotnetify.react.connect("Players", this);
-       return { PlayersList: [] };
-       // The VM's initial state was generated server-side and included with the JSX.
-    },
+class PlayersInTeamTable extends React.Component {
+    constructor(props) {
+       super(props);
+        dotnetify.hubServerUrl = "http://localhost:5000";
+        this.state = dotnetify.react.connect("Players", this);
+        this.state = { PlayerList: []}
+        // Set up function to dispatch state to the back-end with optimistic update.
+        this.dispatch = state => this.vm.$dispatch(state);
+        this.dispatchState = state => {
+            this.setState(state);
+            this.vm.$dispatch(state);
+      }
+    }
     render() {
        return (
-          <div class="table-responsive">
-            <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Pricechange</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {console.log(this)}
-                {console.log(this.state)}
-                {console.log("list", this.state.PlayerList)}
-                    <tr class="">
-                        <td>player.first_name player.second_name</td>
-                        <td>player.cost_change_event</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-       );
+           <PlayerTable data={this.state.PlayerList}/>
+        );
     }
- });
+ };
  
+ class PlayerTable extends React.Component {
+    render() {
+        const isLoading = () => {
+            if(!this.props.data){
+                return <h1>LOADING</h1>
+            } else {
+                return <h1>DONE LOADING</h1>
+            }
+        }
+        const playerRows = this.props.data.map(player => {
+            return <PlayerRow data={player} key={player.id}/>
+        });
+        return (
+            <div className="table-responsive">
+            {isLoading()}
+                <table className="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Pricechange</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {playerRows}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+class PlayerRow extends React.Component {
+    render() {
+        const player = this.props.data;
+        return (
+            <tr className="">
+                <td>{player.first_name} {player.second_name}</td>
+                <td>{player.cost_change_event}</td>
+            </tr>
+        );
+    }
+}
+
  ReactDOM.render(
-       <Players />,
+       <PlayersInTeamTable/>,
    document.getElementById('Content')
  );
